39d5d7494f7524ad6fc676b8fb70b776974c94bc
Tanuja <tanujatiwari04@gmail.com>
2022-07-18 12:38:29 +0530

    add deadline and startdate

--- controllers/index.js
+++ controllers/index.js
@@ -6,18 +6,23 @@ module.exports.all = async (req, res) => {
 }
 
 module.exports.newTodo = async (req, res) => {
-    let resp = await pool.query(`insert into todo(todo) values ('${req.body.todo}')`)
+    let ts = new Date();
+    let date = ts.getDate();
+    let month = ts.getMonth();
+    let year = ts.getFullYear();
+    let currDate = `${year}-${month}-${date}`;
+    let resp = await pool.query(`insert into todo(todo_name, deadline, created_on) values ('${req.body.todo}','${req.body.deadline}','${currDate}')`)
     res.redirect('/todo/all')
 }
 
 module.exports.deleteTodo = async (req, res) => {
-    let resp = await pool.query(`delete from todo where id='${req.params.id}'`)
+    let resp = await pool.query(`delete from todo where todo_id='${req.params.id}'`)
     res.redirect('/todo/all')
 }
 
 module.exports.updateTodo = async (req, res) => {
     let iscompleted = req.body.isCompleted ? req.body.isCompleted : false
-    let resp = await pool.query(`update todo set todo='${req.body.todo}', iscompleted=${iscompleted} where id='${req.params.id}'`)
+    let resp = await pool.query(`update todo set todo_name='${req.body.todo}', iscompleted=${iscompleted} where todo_id='${req.params.id}'`)
     res.redirect('/todo/all')
 }
 
