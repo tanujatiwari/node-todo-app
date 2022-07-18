df1374504abc73820242de28dc4b28e4ea981a40
Tanuja <tanujatiwari04@gmail.com>
2022-07-18 14:30:01 +0530

    add try catch blocks

--- controllers/index.js
+++ controllers/index.js
@@ -1,34 +1,54 @@
 const pool = require('../models/todo')
 
 module.exports.all = async (req, res) => {
-    let resp = await pool.query(`select * from todo order by deadline`)
-    res.send(resp.rows)
+    try {
+        let resp = await pool.query(`select * from todo order by deadline, created_on desc`)
+        res.send(resp.rows)
+    } catch (err) {
+        res.send('An error occured')
+    }
 }
 
 module.exports.newTodo = async (req, res) => {
-    let ts = new Date();
-    let date = ts.getDate();
-    let month = ts.getMonth();
-    let year = ts.getFullYear();
-    let currDate = `${year}-${month}-${date}`;
-    let resp = await pool.query(`insert into todo(todo_name, deadline, created_on) values ('${req.body.todo}','${req.body.deadline}','${currDate}')`)
-    res.redirect('/todo/all')
+    try {
+        let ts = new Date();
+        let date = ts.getDate();
+        let month = ts.getMonth();
+        let year = ts.getFullYear();
+        let currDate = `${year}-${month}-${date}`;
+        let resp = await pool.query(`insert into todo(todo_name, deadline, created_on) values ('${req.body.todo}','${req.body.deadline}','${currDate}')`)
+        res.redirect('/todo/all')
+    } catch (err) {
+        res.send('Something went wrong')
+    }
 }
 
 module.exports.deleteTodo = async (req, res) => {
-    let resp = await pool.query(`delete from todo where todo_id='${req.params.id}'`)
-    res.redirect('/todo/all')
+    try {
+        let resp = await pool.query(`delete from todo where todo_id='${req.params.id}'`)
+        res.redirect('/todo/all')
+    } catch (e) {
+        res.send('An error occured')
+    }
 }
 
 module.exports.updateTodo = async (req, res) => {
-    let iscompleted = req.body.isCompleted ? req.body.isCompleted : false
-    let resp = await pool.query(`update todo set todo_name='${req.body.todo}', iscompleted=${iscompleted} where todo_id='${req.params.id}'`)
-    res.redirect('/todo/all')
+    try {
+        let iscompleted = req.body.isCompleted ? req.body.isCompleted : false
+        let resp = await pool.query(`update todo set todo_name='${req.body.todo}', iscompleted=${iscompleted} where todo_id='${req.params.id}'`)
+        res.redirect('/todo/all')
+    } catch (e) {
+        res.send(e)
+    }
 }
 
 module.exports.searchTodo = async (req, res) => {
-    let resp = await pool.query(`select * from todo where todo_name ilike '%${req.query.q}%'`)
-    res.send(resp.rows)
+    try {
+        let resp = await pool.query(`select * from todo where todo_name ilike '%${req.query.q}%'`)
+        res.send(resp.rows)
+    } catch (e) {
+        res.send('Something went wrong')
+    }
 }
 
 module.exports.notFound = (req, res) => {
