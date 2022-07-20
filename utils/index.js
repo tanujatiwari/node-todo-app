22e666658ad44f9dfb8b3dd683dd16b93aac9318
Tanuja-rs <tanuja.tiwari@remotestate.com>
2022-07-20 16:21:16 +0530

    edit date

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 module.exports.currDate = async () => {
     let ts = new Date();
     let date = ts.getDate();
-    let month = ts.getMonth(); ``
+    let month = ts.getMonth();
     let year = ts.getFullYear();
     return year + "-" + month + "-" + date;
 }
